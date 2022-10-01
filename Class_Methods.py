c69c1479b781401a53a9fe82c25177843f24f965
Jasper Bemmelen <j.vanbemmelen-1@tudelft.nl>
2022-10-01 11:31:56 +0200

    fixed temperature constraints

--- Class_Methods.py
+++ Class_Methods.py
@@ -701,7 +701,9 @@ def check_primer_feasibility(sequences, amplicons, primer_index, optimize=0, tem
         #Temperature constraints
         for primer in amplicon.full_primerset['forward']: #iterate over forward primers
             model.addConstr( min_temp <= primer_index.index2primer['forward'][primer].temperature * (3 - 2 * forward_primers[primer][0]) )
+            model.addConstr( max_temp >= primer_index.index2primer['forward'][primer].temperature * forward_primers[primer][0] )
         for primer in amplicon.full_primerset['reverse']:
+            model.addConstr( min_temp <= primer_index.index2primer['reverse'][primer].temperature * (3 - 2 * forward_primers[primer][0]) )
             model.addConstr( max_temp >= primer_index.index2primer['reverse'][primer].temperature * reverse_primers[primer][0] )
     model.addConstr(max_temp - min_temp <= temperature_range)
         
