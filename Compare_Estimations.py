34a4a28966888aeeb7cb3ef61420cd2c0b0def09
Jasper Bemmelen <j.vanbemmelen-1@tudelft.nl>
2023-04-05 09:43:28 +0200

    added functionality

--- Compare_Estimations.py
+++ Compare_Estimations.py
@@ -26,12 +26,14 @@ def main():
         simset_lineages.add(sequence.lineage)
         if sequence.lineage not in real_abundances:
             real_abundances[sequence.lineage] = 0
-        real_abundances[sequence.lineage] += 1/len(sequences)
+        real_abundances[sequence.lineage] += 100/len(sequences) #this is already the percentage
     #Read estimated abundances
     with open(args.abundances_path, 'r') as f:
         for line in f:
             line = line.split('\t')
-            print(line)
+            if line[0].strip() not in estimated_abundances:
+                estimated_abundances[line[0].strip()] = float(line[-1].strip())
+    print(estimated_abundances)
     print(real_abundances)
 if __name__ == '__main__':
     main()
