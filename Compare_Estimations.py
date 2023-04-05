b251971cafe3e4ec67b2fa80727437780e4372f3
Jasper Bemmelen <j.vanbemmelen-1@tudelft.nl>
2023-04-05 10:00:15 +0200

    print statements

--- Compare_Estimations.py
+++ Compare_Estimations.py
@@ -13,14 +13,19 @@ def main():
     sequences = generate_sequences(args.sequences_path, args.metadata_path)
     
     #Store real and estimated abundances, and errors
-    real_abundances = {}
-    estimated_abundances = {}
-    errors = {}
+    real_abundances = {} #at lineage level
+    real_super_abundances = {} #at super lineage level
+    estimated_abundances = {} #at lineage level
+    estimated_super_abundances = {} #at super lineage level
+    errors = {} #difference between estimated and actual abundance
+    super_errors = {} #same as errors, but now at super lineage level (e.g. BA.1 instead of BA.1.1)
+    
     #Store lineages occurring in simulation data (i.e. the sequences) or in the reference data (based on estimates)
     lineage_mapping = {} #just an index based map of the lineages (e.g. lineage X -> 0)
     all_lineages = set()
     simset_lineages = set()
     refset_lineages = set()
+    
     #Determine actual abundances
     for sequence in sequences:
         all_lineages.add(sequence.lineage)
@@ -28,6 +33,7 @@ def main():
         if sequence.lineage not in real_abundances:
             real_abundances[sequence.lineage] = 0
         real_abundances[sequence.lineage] += 100/len(sequences) #this is already the percentage
+        
     #Read estimated abundances
     with open(args.abundances_path, 'r') as f:
         for line in f:
@@ -36,6 +42,7 @@ def main():
                 all_lineages.add(line[0].strip())
                 if line[0].strip() not in estimated_abundances:
                     estimated_abundances[line[0].strip()] = float(line[-1].strip())
+                    
     #Store lineages that are both in reference set and simulation set
     intersected_lineages = list(simset_lineages.intersection(refset_lineages))
     #Calculate difference between estimated and real abundances
@@ -46,7 +53,9 @@ def main():
             errors[lineage] = -real_abundances[lineage]
         else:
             errors[lineage] = estimated_abundances[lineage] - real_abundances[lineage]
-    print(errors)
+    
+    for lineage in intersected_lineages:
+        print(lineage, errors[lineage])
         
 if __name__ == '__main__':
     main()
