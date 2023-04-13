b82894b0e630d29f1b110b6b212460ea061f73ba
Jasper Bemmelen <j.vanbemmelen-1@tudelft.nl>
2023-04-13 10:58:14 +0200

    debugging

--- Plot_Errors.py
+++ Plot_Errors.py
@@ -19,12 +19,14 @@ def main():
     args = parser.parse_args()
     
     lineages = []
+    super_lineages = []
     errors_wgs = {}
     errors_super_wgs = {}
     errors_amp = {}
     errors_super_amp = {}
     
     max_error = 0
+    max_super_error = 0
     
     #Initialize error dicts and fill lineages list with lineages that are both in refset and simset
     with open(args.intersect, 'r') as f:
@@ -36,6 +38,7 @@ def main():
                 errors_amp[line] = np.zeros((args.num_seeds))
                 
     for seed in range(1,args.num_seeds+1):
+        #Find "normal" errors
         with open(args.amplicon_input + '/Seed_' + str(seed) + '/results/estimation_errors.csv', 'r') as f:
             for line in f:
                 line = line.strip()
@@ -52,6 +55,28 @@ def main():
                     if line[0] in lineages:
                         errors_wgs[line[0]][seed-1] = float(line[1])
                         max_error = max(max_error, abs(float(line[1])))
+                        
+        #Find "super" errors
+        with open(args.amplicon_input + '/Seed_' + str(seed) + '/results/estimation_errors_super.csv', 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line != '':
+                    line = line.split(';')
+                    if line[0] not in super_lineages:
+                        super_lineages.append(line[0])
+                        errors_super_amp[line[0]] = np.zeros((args.num_seeds))
+                    errors_super_amp[line[0]][seed-1] = float(line[1])
+                    max_super_error = max(max_super_error, abs(float(line[1])))
+        with open(args.wgs_input + '/Seed_' + str(seed) + '/results/estimation_errors_super.csv', 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line != '':
+                    line = line.split(';')
+                    if line[0] not in super_lineages:
+                        super_lineages.append(line[0])
+                        errors_super_wgs[line[0]] = np.zeros((args.num_seeds))
+                    errors_super_wgs[line[0]][seed-1] = float(line[1])
+                    max_super_error = max(max_super_error, abs(float(line[1])))
                     
     print('Lineages')
     print(lineages)
@@ -64,6 +89,7 @@ def main():
     print('AMP errors')
     print(errors_amp)
     
+    #Lineage level plots
     fig = plt.figure()
     ax = plt.axes()
     
@@ -84,9 +110,34 @@ def main():
     plt.ylim(-max_error-2.5, max_error+2.5)
     plt.tight_layout()
     
-    plt.grid(axis='y', color='0.95')
+    plt.grid(axis='y', color='1.2')
     
-    plt.savefig('boxcompare.png')
+    plt.savefig('boxcompare_lineage.png')
+    
+    #Super lineage level plots
+    fig = plt.figure()
+    ax = plt.axes()
+    
+    data_amp = [list(errors_super_amp[lineage]) for lineage in super_lineages]
+    data_wgs = [list(errors_super_wgs[lineage]) for lineage in super_lineages]
+    
+    bp_left = plt.boxplot(data_wgs, positions=np.array(range(len(data_amp)))*2.0-0.4, sym='', widths=0.6)
+    bp_right = plt.boxplot(data_amp, positions=np.array(range(len(data_wgs)))*2.0+0.4, sym='', widths=0.6)
+    set_box_color(bp_left, 'red')
+    set_box_color(bp_right, 'blue')
+    
+    plt.plot([], color='red', label='WGS')
+    plt.plot([], color='blue', label='AMP')
+    plt.legend()
+    
+    plt.xticks(range(0, len(super_lineages)*2, 2), super_lineages, rotation=90)
+    plt.xlim(-2, len(super_lineages)*2)
+    plt.ylim(-max_error-2.5, max_error+2.5)
+    plt.tight_layout()
+    
+    plt.grid(axis='y', color='1.2')
+    
+    plt.savefig('boxcompare_superlineage.png')
 
 if __name__ == '__main__':
     main()
