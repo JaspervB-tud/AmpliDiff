9e2a9bea44c6cbb03501622c749821f10ad49876
Jasper Bemmelen <j.vanbemmelen-1@tudelft.nl>
2023-04-17 15:24:31 +0200

    bugfix

--- Calculate_Stats.py
+++ Calculate_Stats.py
@@ -45,16 +45,16 @@ def main():
                     MSE[(coverage, amplicon_width, amplicons, depth)] = (np.mean(cur_values), np.std(cur_values, ddof=1))
                     
     header = 'Depth;'
-    for coverage in coverages:
-        for amplicon_width in amplicon_widths:
+    for amplicon_width in amplicon_widths:
+        for coverage in coverages:
             for amplicon in num_amplicons:
                 header += 'cov-' + coverage + ' width-' + amplicon_width + ' namps-' + amplicon + ';'
     with open(args.output + '/MSE_stats.csv', 'w') as f:
         f.write(header[:-1] + '\n')
         for depth in depths:
             cur_line = '' + depth + ';'
-            for coverage in coverages:
-                for amplicon_width in amplicon_widths:
+            for amplicon_width in amplicon_widths:
+                for coverage in coverages:
                     for amplicon in num_amplicons:
                         cur_line += 'mean=' + str(MSE[(coverage, amplicon_width, amplicons, depth)][0]) + ', std=' + str(MSE[(coverage, amplicon_width, amplicons, depth)][1]) + ';'
             f.write(cur_line[:-1] + '\n')
@@ -62,8 +62,8 @@ def main():
         f.write(header[:-1] + '\n')
         for depth in depths:
             cur_line = '' + depth + ';'
-            for coverage in coverages:
-                for amplicon_width in amplicon_widths:
+            for amplicon_width in amplicon_widths:
+                for coverage in coverages:
                     for amplicon in num_amplicons:
                         cur_line += 'mean=' + str(MAE[(coverage, amplicon_width, amplicons, depth)][0]) + ', std=' + str(MAE[(coverage, amplicon_width, amplicons, depth)][1]) + ';'
             f.write(cur_line[:-1] + '\n')
