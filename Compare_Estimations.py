4582fa46a85fc6e8c90e566f1cf9ee2c4c40b42c
Jasper Bemmelen <j.vanbemmelen-1@tudelft.nl>
2023-04-17 08:39:56 +0200

    added pango lineage clarification and updated version of determining super lineages

--- Compare_Estimations.py
+++ Compare_Estimations.py
@@ -1,6 +1,41 @@
 from Fetch_Amplicons import generate_sequences
 import argparse
 
+def extract_lineages(PANGO_lineages_file):
+    aliases = {}
+    with open(PANGO_lineages_file, 'r') as f:
+        next(f)
+        for line in f:
+            if line[0] != '*': #if the line starts with an asterisk, the lineage has been withdrawn
+                cur_line = line.split()
+                if 'alias' in line or 'Alias' in line: #if the line contains alias, then the current lineage is an alias
+                    aliases[cur_line[0]] = cur_line[3].replace(',', '')
+                else:
+                    aliases[cur_line[0]] = cur_line[0]
+    return aliases
+
+def calculate_errors(estimated_abundances, real_abundances, aliases, depth=1000):
+    errors = {}
+    
+    for lineage in estimated_abundances:
+        current_lineage = '.'.join(aliases[lineage].split('.')[:depth])
+        if current_lineage not in errors:
+            errors[current_lineage] = 0
+        if lineage not in real_abundances:
+            errors[current_lineage] += estimated_abundances[lineage]
+        else:
+            errors[current_lineage] += estimated_abundances[lineage] - real_abundances[lineage]
+    for lineage in real_abundances:
+        current_lineage = '.'.join(aliases[lineage].split('.')[:depth])
+        if current_lineage not in errors:
+            errors[current_lineage] = 0
+        if lineage not in estimated_abundances:
+            errors[current_lineage]  += -real_abundances[lineage]
+        else:
+            errors[current_lineage] += estimated_abundances[lineage] - real_abundances[lineage]
+    
+    return errors
+
 def determine_superlineages(lineages):
     super_lineages = {}
     for lineage in lineages:
@@ -53,10 +88,49 @@ def main():
     parser.add_argument('-s', '--sequences_path', type=str, help='Sequences used for simulation file location', required=True)
     parser.add_argument('-m', '--metadata_path', type=str, help='Metadata for sequences used in simulation file location', required=True)
     parser.add_argument('-a', '--abundances_path', type=str, help='TSV file containing the estimated abundances', required=True)
+    parser.add_argument('--pango', type=str, help='File containing the PANGO lineage information', required=True)
     parser.add_argument('-o', '--output_folder', type=str, help='Path to the folder where output will be saved', default='.')
     
     args = parser.parse_args()
-    sequences = generate_sequences(args.sequences_path, args.metadata_path)
+    sequences = generate_sequences(args.sequences_path, args.metadata_path) #store simulated sequences (abundance equal to occurrence)
+    aliases = extract_lineages(args.pango) #store lineages and their translations
+    
+    real_abundances = {} #lineage level simulated abundances
+    estimated_abundances = {} #lineage level extimated abundances
+    
+    simulated_lineages = set() #all lineages in the simulated dataset
+    estimated_lineages = set() #all lineages in the reference set
+    intersect_lineages = set() #lineages that are both in simulated and reference set
+    
+    max_depth = 0 #maximum "length" of a lineage (e.g. B.1.1.117 has depth 4)
+    
+    #Determine simulated abundances
+    for sequence in sequences:
+        if sequence.lineage not in real_abundances:
+            real_abundances[sequence.lineage] = 0
+        real_abundances[sequence.lineage] += 100/len(sequences) #percentage of sequences per lineage
+        simulated_lineages.add(sequence.lineage)
+        max_depth = max(max_depth, len(aliases[sequence.lineage].split('.')))
+    #Determine estimated abundances
+    with open(args.abundances_path, 'r') as f:
+        for line in f:
+            if not '#' in line:
+                line = line.split('\t')
+                estimated_abundances[line[0].strip()] = float(line[-1].strip())
+                estimated_lineages.add(line[0].strip())
+                max_depth = max(max_depth, len(aliases[line[0].strip()].split('.')))
+    #Determine lineages occurring in both simulation and ref set
+    intersect_lineages = simulated_lineages.intersection(estimated_lineages)
+    
+    #Iterate over different depths, calculate errors and store them
+    for depth in range(1, max_depth+1):
+        errors = calculate_errors(estimated_abundances, real_abundances, aliases, depth=depth)
+        
+    
+    
+    
+    
+    
     
     #Store real and estimated abundances, and errors
     real_abundances = {} #at lineage level
